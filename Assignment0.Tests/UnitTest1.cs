71840bc97c42c50e124f221aa35b1e125c9a3548
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-09-06 15:04:15 +0200

    Added user interface for LeapYear

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -3,8 +3,24 @@ namespace Assignment0.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void IsDivisibleByFour()
     {
-
+        var LeapYear = new LeapYear();
+        //Asserts
+        Assert.True(LeapYear.IsLeapYear(1996));
+    }
+    [Fact]
+    public void isDivisibleByFourHundred()
+    {
+        var LeapYear = new LeapYear();
+        //Asserts
+        Assert.True(LeapYear.IsLeapYear(2000));
+    }
+    [Fact]
+    public void isDivisibleByOneHundred()
+    {
+        var LeapYear = new LeapYear();
+        //Asserts
+        Assert.False(LeapYear.IsLeapYear(1900));
     }
 }
--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class LeapYear
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a year:");
+        string temp = Console.ReadLine();
+        int input;
+        if(int.TryParse(temp, out input)){
+            if (new LeapYear().IsLeapYear(input))
+        {
+            Console.WriteLine("yay");
+        }
+        else
+        {
+            Console.WriteLine("nay");
+        }
+        } else {
+            Console.WriteLine("Input was not a year.");
+        }
+    }
+
+    public bool IsLeapYear(int year)
+    {
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
