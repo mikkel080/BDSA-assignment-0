b1c3348e9b673f7424a269a33646d26f4aed9f72
Mikkel Bistrup Andersen <mbia@itu.dk>
2022-09-09 10:02:44 +0200

    Added documentation and test cases

--- Assignment0.Tests/UnitTest1.cs
+++ Assignment0.Tests/UnitTest1.cs
@@ -23,4 +23,42 @@ public class UnitTest1
         //Asserts
         Assert.False(LeapYear.IsLeapYear(1900));
     }
+
+    [Fact]
+    public void UserInputIsInteger()
+    {
+        var LeapYear = new LeapYear();
+        var writer = new StringWriter();
+        var reader = new StringReader("Not a year");
+        Console.SetOut(writer);
+        Console.SetIn(reader);
+
+        var program = Assembly.Load(nameof(Assignment0));
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        //Asserts
+        var tempactual = writer.GetStringBuilder().ToString().TrimEnd();
+        string[] listoftempactual = tempactual.Split('\n');
+        var actual = listoftempactual[1];
+        Assert.Equal("Input was not a year.", actual);
+    }
+
+     [Fact]
+    public void UserInputIsafter1582()
+    {
+        var LeapYear = new LeapYear();
+        var writer = new StringWriter();
+        var reader = new StringReader("1582");
+        Console.SetOut(writer);
+        Console.SetIn(reader);
+
+        var program = Assembly.Load(nameof(Assignment0));
+        program.EntryPoint?.Invoke(null, new[] { Array.Empty<string>() });
+
+        //Asserts
+        var tempactual = writer.GetStringBuilder().ToString().TrimEnd();
+        string[] listoftempactual = tempactual.Split('\n');
+        var actual = listoftempactual[1];
+        Assert.Equal("Please time a year after 1582.", actual);
+    }
 }
--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -1,20 +1,28 @@
 ﻿public class LeapYear
 {
-    public static void Main()
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Enter a year:");
+        Console.WriteLine("Enter a year after 1582:");
         string temp = Console.ReadLine();
         int input;
-        if(int.TryParse(temp, out input)){
-            if (new LeapYear().IsLeapYear(input))
+        if (int.TryParse(temp, out input))
         {
-            Console.WriteLine("yay");
+            if (input > 1582)
+            {
+                if (new LeapYear().IsLeapYear(input))
+                {
+                    Console.WriteLine("yay");
+                }
+                else
+                {
+                    Console.WriteLine("nay");
+                }
+            } else {
+                Console.WriteLine("Please time a year after 1582.");
+            }
         }
         else
         {
-            Console.WriteLine("nay");
-        }
-        } else {
             Console.WriteLine("Input was not a year.");
         }
     }
